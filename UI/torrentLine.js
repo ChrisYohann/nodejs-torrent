5e2cd814be98896f0aa47ea9caba5656d0ab3a93
ChrisYohann <i.nhyne@gmail.com>
2017-11-01 13:01:51 +0100

    Added Fix in TorrentLine

--- UI/torrentLine.js
+++ UI/torrentLine.js
@@ -10,25 +10,30 @@ let LineBuffer = CLI.LineBuffer;
 let Progress = CLI.Progress;
 
 let TorrentLine = module.exports = function(torrent){
+    console.log("Creating TorrentLine");
     let self = this ;
     EventEmitter.call(this);
     this.torrent = torrent;
     this.content = this.updateLineContent();
-    torrent.on("change", function(torrent){
+    this.torrent.on("change", function(torrent){
         self.content = self.updateLineContent();
         self.emit("torrentChange", self.content);
     });
+
 
 };
 
 util.inherits(TorrentLine, EventEmitter);
 
 TorrentLine.prototype.updateLineContent = function(){
-    return new Line()
+   let result = new Line()
         .padding(4)
-        .column(torrentObject["name"], Math.ceil(0.25*NB_COLUMNS))
+        .column(this.torrent["name"], Math.ceil(0.25*NB_COLUMNS))
         .column(new Progress(Math.ceil(0.20*NB_COLUMNS)).update(this.torrent["_completed"], this.torrent["_size"]))
         .column('Speed', Math.ceil(0.15*NB_COLUMNS))
         .column('0', Math.ceil(0.15*NB_COLUMNS))
         .fill();
+  return result;
+
+
 };
